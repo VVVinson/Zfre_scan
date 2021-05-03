da99b48267aff81e0da62b877b10574182275e49
VVVinson <459789875@qq.com>
2021-05-03 14:09:54 +0800

    update

--- module/ZoomEye.py
+++ module/ZoomEye.py
@@ -1,5 +1,4 @@
 #coding:utf-8
-# import zoomeye.sdk as zoomeye
 from openpyxl import Workbook
 import time
 import requests
@@ -7,13 +6,20 @@ import json
 import optparse
 import re
 import socket
+from urllib.parse import quote
+from requests.adapters import HTTPAdapter
 
 result_list = []
 
 def search_zoomeye(dork, page, apikey):
+    dork = quote(dork, 'utf-8')
+    s = requests.Session()
+    s.mount('http://', HTTPAdapter(max_retries=5))
+    s.mount('https://', HTTPAdapter(max_retries=5))
     burp0_url = "https://api.zoomeye.org/host/search?query={}&page={}".format(dork, page)
     burp0_headers = {"API-KEY": apikey}
-    result = requests.get(burp0_url, headers=burp0_headers)
+    print(burp0_url)
+    result = s.get(burp0_url, headers=burp0_headers, timeout=10)
     if result.status_code == 200:
         total = json.loads(result.text)["total"]
         matches = json.loads(result.text)["matches"]
@@ -24,12 +30,15 @@ def search_zoomeye(dork, page, apikey):
             portinfo = each_match["portinfo"]
             port = portinfo["port"]
             service = portinfo["service"]
+            if "http" in service:
+                service = "http"
+            elif "https" in service:
+                service = "https"
             try:
                 title = str(portinfo["title"])
             except:
                 title = ""
             url = "{}://{}:{}".format(service, IP, port)
-            # if "http" not in service:            #非http协议telnet一下
             test_telnet_result = test_socket(IP, port)      #socket连接测试端口通不通
             if test_telnet_result == 1:
                 result_list.append([url, service, title])
@@ -42,6 +51,7 @@ def search_zoomeye(dork, page, apikey):
 
     else:           #非200响应码返回错误信息
         print(result.status_code, result.text)
+
 
 def toexcel(result_list):
     wb = Workbook()
@@ -84,39 +94,48 @@ def get_apikey():
                 continue
     return apikey_list
 
-def main(Zoomeye_file_path):
-    apikey_list = get_apikey()
-    apikey_len = len(apikey_list)
-    apikey_index = 0
-    with open(Zoomeye_file_path, "r",encoding='utf-8') as f:          #读取关键字
-        for line in f.readlines():
-            line = line.strip('\n').strip()  # 去掉列表中每一个元素的换行符
-            if line == "":        # 有空行会返回500导致报错
-                continue
-            total = search_zoomeye(line, 1, apikey_list[apikey_index])       #获取第一页数据，并获取total
-            if total == None:                    #该key余额已用完
-                apikey_index += 1                #切换到下一个key
-                if apikey_len == apikey_index:    #所有key余额已用完
+def main(Zoomeye_file_path, add_pram):
+    try:
+        apikey_list = get_apikey()
+        apikey_len = len(apikey_list)
+        apikey_index = 0
+        with open(Zoomeye_file_path, "r",encoding='utf-8') as f:          #读取关键字
+            for line in f.readlines():
+                line = line.strip('\n').strip()  # 去掉列表中每一个元素的换行符
+                if line == "":        # 有空行会返回500导致报错
+                    continue
+                else:
+                    line = line + " " + add_pram
+                if apikey_list == []:
                     print("APIKey一滴也没有了")
                     break
-                else:
-                    total = search_zoomeye(line, 1, apikey_list[apikey_index])          #使用下一个key
-            if total <= 20:                 #不用翻页
-                continue
-            else:                            #通过total判断是否需要翻页
-                page_num = total//20 + 1      #计算翻页的页数
-                print("[+]Zoomeye总共页数:" + str(page_num))
-                for page in range(2, page_num + 1):
-                    print("[+]Zoomeye目前页数:" + str(page))
-                    total = search_zoomeye(line, page, apikey_list[apikey_index])
-                    if total == None:          #该key余额已用完
-                        apikey_index += 1           #切换到下一个key
-                        if apikey_len == apikey_index:  #所有key余额已用完
-                            print("APIKey一滴也没有了")
-                            break
-                        else:
-                            search_zoomeye(line, page, apikey_list[apikey_index])          #使用下一个key
-            print(result_list)
+                total = search_zoomeye(line, 1, apikey_list[apikey_index])       #获取第一页数据，并获取total
+                if total == None:                    #该key余额已用完
+                    apikey_index += 1                #切换到下一个key
+                    if apikey_len == apikey_index:    #所有key余额已用完
+                        print("APIKey一滴也没有了")
+                        break
+                    else:
+                        total = search_zoomeye(line, 1, apikey_list[apikey_index])          #使用下一个key
+                if total <= 20:                 #不用翻页
+                    continue
+                else:                            #通过total判断是否需要翻页
+                    page_num = total//20 + 1      #计算翻页的页数
+                    print("[+]Zoomeye总共页数:" + str(page_num))
+                    for page in range(2, page_num + 1):
+                        print("[+]Zoomeye目前页数:" + str(page))
+                        total = search_zoomeye(line, page, apikey_list[apikey_index])
+                        if total == None:          #该key余额已用完
+                            apikey_index += 1           #切换到下一个key
+                            if apikey_len == apikey_index:  #所有key余额已用完
+                                print("APIKey一滴也没有了")
+                                break
+                            else:
+                                search_zoomeye(line, page, apikey_list[apikey_index])          #使用下一个key
+                print(result_list)
+    except Exception as e:
+        print(str(e))
+
     totxt(result_list)
     toexcel(result_list)
 
